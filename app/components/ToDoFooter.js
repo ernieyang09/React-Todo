9e6b8a25c41f055b52bc9eda1bb2aea387167ba0
Ernie Yang <ernieyang09@hotmail.com>
2017-05-15 17:35:02 +0800

    ToDoFooter Stateless

--- app/components/ToDoFooter.js
+++ app/components/ToDoFooter.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ToDoAction from '../actions/ToDoAction';
 
-class ToDoFooter extends Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    const { ToDoTotal,DoneCount } = this.props;
-    return (
-       <div>
-        總共 {ToDoTotal} 已完成 {DoneCount} 未完成 {ToDoTotal-DoneCount}
-      </div>
-    )
-  }
+const ToDoFooter = (props) =>{
+  const { ToDoTotal,DoneCount } = props;
+  return (
+     <div>
+      總共 {ToDoTotal} 已完成 {DoneCount} 未完成 {ToDoTotal-DoneCount}
+    </div>
+  )
 }
-
 
 ToDoFooter.propTypes = {
     ToDoTotal:PropTypes.number.isRequired,
