a9d9a54941c3227ba4e42787b6097b3706817b7f
Ernie Yang <ernieyang09@hotmail.com>
2017-05-15 17:33:21 +0800

    ToDoList Stateless

--- app/components/ToDoList.js
+++ app/components/ToDoList.js
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 import ToDoAction from '../actions/ToDoAction';
 import ToDoItem from '../components/ToDoItem';
 
-
-class ToDoList extends Component {
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return(
-      <ul>
-        {
-          this.props.ToDoItems.map((ToDo,index)=> <ToDoItem  key={index} {...ToDo}/> )
-        }
-      </ul>
-    )
-  }
+const ToDoList = (props) =>{
+        return(
+        <ul>
+          {
+            props.ToDoItems.map((ToDo,index)=> <ToDoItem  key={index} {...ToDo}/> )
+          }
+        </ul>
+        )
 }
 
 export default ToDoList;
