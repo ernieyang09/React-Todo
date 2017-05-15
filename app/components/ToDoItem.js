980db165bac3863e7db0eb6da63e46ea419f86b9
Ernie Yang <ernieyang09@hotmail.com>
2017-05-15 19:25:12 +0800

    ToDoItem stateless

--- app/components/ToDoItem.js
+++ app/components/ToDoItem.js
@@ -2,65 +2,52 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ToDoAction from '../actions/ToDoAction';
 
-
-class ToDoItem extends Component {
-  constructor(props){
-    super(props);
-    this.toggleCheckBox = this.toggleCheckBox.bind(this);
-    this.deleteToDo = this.deleteToDo.bind(this);
-    this.toggleEditMode = this.toggleEditMode.bind(this);
-    this.updateContent = this.updateContent.bind(this);
-
+const ToDoItem = (props) => {
+  let editNode;
+  const toggleCheckBox = () => {
+    const checked = !props.checked;
+    ToDoAction.ToDoUpdate({id: props.id, content: props.content, checked : checked});
   }
 
-
-  toggleCheckBox(){
-    const checked = !this.props.checked;
-    ToDoAction.ToDoUpdate({id: this.props.id, content: this.props.content, checked : checked});
+  const toggleEditMode = () => {
+    ToDoAction.ToDoToggleEdit({id: props.id, edit:!props.edit});
   }
 
-  toggleEditMode(){
-    ToDoAction.ToDoToggleEdit({id: this.props.id, edit:!this.props.edit});
-  }
-
-  updateContent(){
-    const editText = this.refs.editText.value.trim();
+  const updateContent = () => {
+    const editText = editNode.value.trim();
     if(editText===''){
       alert('Error:不能為空');
       return;
     }
-    ToDoAction.ToDoUpdate({id: this.props.id, content: editText, checked : this.props.checked});
+    ToDoAction.ToDoUpdate({id: props.id, content: editText, checked : props.checked});
   }
 
-  deleteToDo() {
+  const deleteToDo = () => {
     if(confirm('確定要刪除')) {
-      ToDoAction.ToDoDelete(this.props);
+      ToDoAction.ToDoDelete(props);
     }
   }
 
-  renderEditMode(){
+  const renderEditMode = (props) => {
     return (
       <li>
-        <input type='text' defaultValue = {this.props.content} ref='editText' /> <input type= 'button' value = '確定' onClick={this.updateContent} /> <input type='button' value = '取消' onClick ={this.toggleEditMode}/>
+        <input type='text' defaultValue = {props.content} ref={ node => editNode = node } /> <input type= 'button' value = '確定' onClick={updateContent} /> <input type='button' value = '取消' onClick ={toggleEditMode}/>
       </li>
     )
 
   }
 
-  renderViewMode(){
-    return (
-      <li>
-        <label><input type='checkbox' checked = {this.props.checked} onChange = {this.toggleCheckBox}/> {this.props.content}</label> <input type='button' value='修改' onClick = {this.toggleEditMode}/> <input type='button' value='刪除' onClick = {this.deleteToDo}/>
-      </li>
+  const renderViewMode = (props) =>{
+        return (
+          <li>
+            <label><input type='checkbox' checked = {props.checked} onChange = {toggleCheckBox}/> {props.content}</label> <input type='button' value='修改' onClick = {toggleEditMode}/> <input type='button' value='刪除' onClick = {deleteToDo}/>
+          </li>
 
-    )
+        )
   }
 
-  render(){
-    return (!this.props.edit)?this.renderViewMode():this.renderEditMode();
-  }
+  return(!props.edit)?renderViewMode(props):renderEditMode(props);
+
 }
-
-
 
 export default ToDoItem;
