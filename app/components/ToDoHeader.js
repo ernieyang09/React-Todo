7262803392d8463f122a064933373190270ab46d
Ernie Yang <ernieyang09@hotmail.com>
2017-05-15 17:22:05 +0800

    Stateless ToDoHeader

--- app/components/ToDoHeader.js
+++ app/components/ToDoHeader.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ToDoAction from '../actions/ToDoAction';
 
-class ToDoHeader extends Component {
-  constructor(props){
-    super(props);
-    this.onAdd = this.onAdd.bind(this);
-  }
+const ToDoHeader = (({...props})=>{
+   let txtToDo;
+   const onAdd = () => {
+       let newToDo = txtToDo.value.trim();
+       if(newToDo==='')
+         return;
+       ToDoAction.ToDoCreate(newToDo);
+       txtToDo.value = '';
+   }
 
+   return (<div><input type='text' placeholder = {props.text} ref={node => txtToDo = node }/><input type='button' value='新增' onClick = {onAdd}/></div>)
 
-  onAdd (){
-    let newToDo = this.refs.txtToDo.value.trim();
-    if(newToDo==='')
-      return;
-    ToDoAction.ToDoCreate(newToDo);
-    this.refs.txtToDo.value = '';
-  }
-
-  render(){
-    const {text} = this.props;
-    return <div><input type='text' placeholder = {text} ref='txtToDo'/><input type='button' value='新增' onClick = {this.onAdd}/></div>
-  }
-}
+});
 
 ToDoHeader.propTypes = {
    text:PropTypes.string
